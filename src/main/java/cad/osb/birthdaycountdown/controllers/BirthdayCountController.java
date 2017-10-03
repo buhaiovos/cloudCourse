c747fa183319591146335dc24dc1e0f75663be82
Олександр Бугайов <oleksandr.buhaiov@gmail.com>
2017-10-03 18:48:42 +0300

    continuing to play with locale

--- src/main/java/cad/osb/birthdaycountdown/controllers/BirthdayCountController.java
+++ src/main/java/cad/osb/birthdaycountdown/controllers/BirthdayCountController.java
@@ -8,7 +8,9 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.i18n.CookieLocaleResolver;
 
+import javax.servlet.http.HttpServletRequest;
 import java.text.ParseException;
 import java.util.Date;
 import java.util.Locale;
@@ -24,7 +26,10 @@ public class BirthdayCountController {
 
     @RequestMapping(value = "/countdown", method = RequestMethod.POST)
     public String getBirthDayCountdown(
-            @RequestParam("d") String bDate, Model model, Locale locale) {
+            @RequestParam("d") String bDate, Model model, HttpServletRequest request) {
+        CookieLocaleResolver resolver = new CookieLocaleResolver();
+        resolver.setDefaultLocale(Locale.forLanguageTag("uk-UA"));
+        Locale locale = resolver.resolveLocale(request);
 
         LogManager.getLogger(BirthdayCountController.class)
                 .info("Initiating calculation for " + bDate);
